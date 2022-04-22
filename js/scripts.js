0bde0a157d3a2b5b76b7dbd6258c0d61fac83b36
nicolle-studiocollab <104228144+nicolle-studiocollab@users.noreply.github.com>
2022-04-22 16:27:57 -0400

    Updated Website Including Qualtrics Case Study
    Links to V6 - Collab in local

---
+++ js/scripts.js
@@ -0,0 +1,112 @@
+$(function (){
+    var current_date = new Date();
+    var hour = current_date.getHours();
+
+    if (hour >= 5 && hour < 11) {
+        $('#hero-area').addClass('morning')
+    } else if (hour >= 11 && hour < 16) {
+        $('#hero-area').addClass('afternoon')
+    } else if (hour >= 16 && hour < 20) {
+        $('#hero-area').addClass('evening')
+    }
+    else {
+        $('#hero-area').addClass('night')
+    }
+});
+
+
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+        console.log("OK");
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
+
+/* Contact Us Form Shrinking Headlines */
+
+$(() => {
+
+    $('.form-group').each((i, e) => {
+      $('.form-control', e).
+      focus(function () {
+        e.classList.add('not-empty');
+      }).
+      blur(function () {
+        this.value === '' ? e.classList.remove('not-empty') : null;
+      });
+  
+    });
+  
+  });
+
+
+/*!
+    * Start Bootstrap - Agency v6.0.3 (https://startbootstrap.com/theme/agency)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+    */
+   (function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top - 72,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+})(jQuery); // End of use strict
+
+
